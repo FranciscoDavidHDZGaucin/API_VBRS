c9577362cad967db4aa709ff242c4245da037985
fhernandez <fhernandez@VDI-TIC03>
2020-01-13 08:48:09 -0600

    agregado de RECLAMOSERVICIOController funcionana

---
+++ OTRA_API_008/Controllers/RECLAMOSERVICIOController.cs
@@ -0,0 +1,24 @@
+﻿using OTRA_API_008.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OTRA_API_008.Controllers
+{
+    public class RECLAMOSERVICIOController : ApiController
+    {
+
+        public int Get(int folio)
+        {
+
+            RECLAMOS_BD_MAIL SENRECLAMO = new RECLAMOS_BD_MAIL();
+            return SENRECLAMO.SEND_MAIL_SERVICIO(folio);
+
+        }
+
+
+    }
+}
--- OTRA_API_008/Models/RECLAMOS_BD_MAIL.cs
+++ OTRA_API_008/Models/RECLAMOS_BD_MAIL.cs
@@ -45,7 +45,45 @@ namespace OTRA_API_008.Models
 
             return resultado;
         }
+        public int SEND_MAIL_SERVICIO(int FOLIO)
+        {
+            int resultado = 00;
+            try
+            {
+                using (SqlConnection CONNECT = new SqlConnection(@"Data Source=SRV-DBGW;Initial Catalog=JUPITER;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONNECT.Open();
+                    using (SqlCommand commando = new SqlCommand("SP_SAC_SERVICIO_EMAIL", CONNECT))
+                    {
+                        commando.CommandType = CommandType.StoredProcedure;
+                        commando.Parameters.Add("@FOLIOBRS", SqlDbType.Int);
 
+                        commando.Parameters["@FOLIOBRS"].Value = FOLIO;
+                        commando.ExecuteScalar();
+
+                        resultado = 88;
+                    }
+
+
+
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                resultado = 23;
+
+
+            }
+
+            return resultado;
+
+
+
+
+        }
 
 
 
