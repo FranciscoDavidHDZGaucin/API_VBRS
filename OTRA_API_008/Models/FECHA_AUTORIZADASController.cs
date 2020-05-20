dc69769b1f2e28017a1d510a9534cf5d9b202ec5
fhernandez <fhernandez@VDI-TIC03>
2020-05-20 08:33:19 -0500

    AGREGADO DE   FECHAS  AUTORIZADAS  RSGD

--- OTRA_API_008/Models/FECHA_AUTORIZADASController.cs
+++ OTRA_API_008/Models/FECHA_AUTORIZADASController.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace OTRA_API_008.Models
 {
     public class FECHA_AUTORIZADASController : ApiController
     {
 
+        [EnableCors(origins: "http://192.168.101.128/", headers: "*", methods: "*")]
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            FECHAS_AUTORIZADAS_RSGD OBJETO_VENTAS = new FECHAS_AUTORIZADAS_RSGD();
 
-       
+
+
+            return OBJETO_VENTAS.CALL_PLATAFORMA_UNES();
+
+        }
+
+
 
 
 
