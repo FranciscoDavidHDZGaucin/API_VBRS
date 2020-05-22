a7cabeec14c658589fc4b3e2bb944f6d6b0d90f3
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2020-05-22 13:21:57 -0500

    APIs VTS DETALLE Y PRESUPUESTO MENSUAL

---
+++ OTRA_API_008/Controllers/API_PRESUPUESTO_MENSUALController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class API_PRESUPUESTO_MENSUALController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            API_PRESUPUESTO_MENSUAL ApiPresu = new API_PRESUPUESTO_MENSUAL();
+            JArray result = new JArray();
+            result = ApiPresu.PRESUPUESTO();
+            return result;
+        }
+
+    }
+}
