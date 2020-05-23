b4c976fe97031bbf6880e68b9e3c0e9dd6e77aa4
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2020-05-23 12:59:01 -0500

    API PRESUPUESTO_VS_GASTOS

---
+++ OTRA_API_008/Controllers/PRESUPUESTO_VS_GASTOSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class PRESUPUESTO_VS_GASTOSController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            PRESUPUESTO_VS_GASTOS PRESUP_GAST = new PRESUPUESTO_VS_GASTOS();
+            JArray result = new JArray();
+            result = PRESUP_GAST.PRESUPUESTO_GASTOS();
+            return result;
+        }
+
+    }
+}
