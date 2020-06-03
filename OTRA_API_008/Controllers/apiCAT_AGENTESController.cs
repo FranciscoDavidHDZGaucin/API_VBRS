6b37d600841410a9252eda515c71bdd08ec6c2a8
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2020-06-03 18:04:00 -0500

    APIS AGENTES Y ALMACENES

---
+++ OTRA_API_008/Controllers/apiCAT_AGENTESController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class apiCAT_AGENTESController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            apiCAT_AGENTES Apigente = new apiCAT_AGENTES();
+            JArray result = new JArray();
+            result = Apigente.AGENTES();
+            return result;
+        }
+    }
+}
