feb4d499d829a8db3b070e37dbc914f4a4a2c0b3
Jorge Alberto Olvejra aszzzzzzzzzz <practicante_tic@agroversa.zmxzzz>
2020-06-04 17:08:25 -0500

    vwCAT_CLIENTES

---
+++ OTRA_API_008/Controllers/vw_CAT_CLIENTESController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+namespace OTRA_API_008.Controllers
+{
+    public class CAT_CLIENTESController : ApiController
+    {
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            vwCAT_CLIENTES CLIENTES = new vwCAT_CLIENTES();
+            JArray result = new JArray();
+            result = CLIENTES.CAT_CLIENTE();
+            return result;
+        }
+
+
+    }
+}
