eb7a9d495dd01035fd9693ae5e375b3b607c5f61
Jorge Alberto Olvejra aszzzzzzzzzz <practicante_tic@agroversa.zmxzzz>
2020-06-04 17:06:38 -0500

    vwCAT_PRODUCTOS

---
+++ OTRA_API_008/Models/vwCAT_PRODUCTOS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Models
+{
+    public class vwCAT_PRODUCTOS
+    {
+
+        private SqlDataReader drd;
+
+        public Newtonsoft.Json.Linq.JArray CAT_PRODUCTOS()
+        {
+
+            DataTable dtvts = new DataTable();
+            try
+            {
+                using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.154;Initial Catalog=JUPITER;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONECT.Open();
+                    using (SqlCommand COMANDO = new SqlCommand("SELECT * FROM vwCAT_PRODUCTOS", CONECT))
+
+
+                    {
+
+                        dtvts.Load(COMANDO.ExecuteReader());
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                dtvts = new DataTable();
+            }
+            var jsonresult = JsonConvert.SerializeObject(dtvts);
+            JArray Areglo_JSON = JArray.Parse(jsonresult);
+
+            return Areglo_JSON;
+        }
+
+    }
+}
