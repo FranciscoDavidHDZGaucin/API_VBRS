85f4800244af6caa1bffd0eaed2f0af3e67ce073
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2020-08-18 14:24:15 -0500

    MODIFICACION API GEP FACTURA

---
+++ OTRA_API_008/Controllers/apiTB_GEP_FACTURAController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class apiTB_GEP_FACTURAController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            apiTB_GEP apiGEP = new apiTB_GEP();
+            JArray result = new JArray();
+            result = apiGEP.GEP();
+            return result;
+        }
+
+    }
+}
---
+++ OTRA_API_008/Models/apiTB_GEP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace OTRA_API_008.Models
+{
+    public class apiTB_GEP
+    {
+
+        private SqlDataReader drd;
+
+        public List<DataTable> _RES_TOTALES = null;
+
+        public Newtonsoft.Json.Linq.JArray GEP()
+        {
+
+            DataTable dtvts = new DataTable();
+            try
+            {
+                using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.154;Initial Catalog=JUPITER;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONECT.Open();
+                    using (SqlCommand COMANDO = new SqlCommand("SELECT DISTINCT FACTURA FROM TABLA_PRINCIPAL_GEP", CONECT))
+
+
+                    {
+                        dtvts.Load(COMANDO.ExecuteReader());
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                dtvts = new DataTable();
+            }
+            var jsonresult = JsonConvert.SerializeObject(dtvts);
+            JArray Areglo_JSON = JArray.Parse(jsonresult);
+
+            return Areglo_JSON;
+        }
+
+    }
+}
