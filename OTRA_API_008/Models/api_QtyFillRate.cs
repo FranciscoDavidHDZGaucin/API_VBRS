e98a81c76548168e179d5c53d644bcc6a510957b
fhernandez <fhernandez@VDI-TIC03>
2021-01-25 16:42:38 -0600

    CREAMOS  class api_QtyFillRate   LA CUAL CONTIEN  LAS  FUENTES  DE  INFORMACION  PARA EL INDICADOR QtyFillRate

---
+++ OTRA_API_008/Models/api_QtyFillRate.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OTRA_API_008.Models
+{
+    public class api_QtyFillRate
+    {
+
+        private SqlDataReader drd;
+
+
+        public Newtonsoft.Json.Linq.JArray QtyFillRate_DETALLE_PEDIDOS()
+        {
+
+            DataTable dtvts = new DataTable();
+            try
+            {
+                using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.22;Initial Catalog=AGROVERSA_PRODUCTIVA;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONECT.Open();
+                    using (SqlCommand COMANDO = new SqlCommand("SELECT * FROM VW_QtyFillRate_DETALLE_PEDIDOS", CONECT))
+
+
+                    {
+
+                        dtvts.Load(COMANDO.ExecuteReader());
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                dtvts = new DataTable();
+            }
+            var jsonresult = JsonConvert.SerializeObject(dtvts);
+            JArray Areglo_JSON = JArray.Parse(jsonresult);
+
+            return Areglo_JSON;
+        }
+
+        public Newtonsoft.Json.Linq.JArray QtyFillRate_FACTURA()
+        {
+
+            DataTable dtvts = new DataTable();
+            try
+            {
+                using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.22;Initial Catalog=AGROVERSA_PRODUCTIVA;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONECT.Open();
+                    using (SqlCommand COMANDO = new SqlCommand("SELECT * FROM VW_QtyFillRate_FACTURA", CONECT))
+
+
+                    {
+
+                        dtvts.Load(COMANDO.ExecuteReader());
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                dtvts = new DataTable();
+            }
+            var jsonresult = JsonConvert.SerializeObject(dtvts);
+            JArray Areglo_JSON = JArray.Parse(jsonresult);
+
+            return Areglo_JSON;
+        }
+
+        public Newtonsoft.Json.Linq.JArray QtyFillRate_REPT_LOG_PEDIDOS()
+        {
+
+            DataTable dtvts = new DataTable();
+            try
+            {
+                using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.22;Initial Catalog=AGROVERSA_PRODUCTIVA;User ID=sa;Password=DB@gr0V3rs@"))
+                {
+                    CONECT.Open();
+                    using (SqlCommand COMANDO = new SqlCommand("SELECT * FROM VW_QtyFillRate_REPT_LOG_PEDIDOS", CONECT))
+
+
+                    {
+
+                        dtvts.Load(COMANDO.ExecuteReader());
+
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                dtvts = new DataTable();
+            }
+            var jsonresult = JsonConvert.SerializeObject(dtvts);
+            JArray Areglo_JSON = JArray.Parse(jsonresult);
+
+            return Areglo_JSON;
+        }
+
+    }
+}
