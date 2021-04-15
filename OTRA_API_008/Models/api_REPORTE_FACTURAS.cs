b744b0746ca0a57f8b9abded5c83183006cabfe7
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2021-04-15 09:31:13 -0500

    Modificacion inventario y reporte de facturas DAFH

--- OTRA_API_008/Models/api_REPORTE_FACTURAS.cs
+++ OTRA_API_008/Models/api_REPORTE_FACTURAS.cs
@@ -24,7 +24,7 @@ namespace OTRA_API_008.Models
                 using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.154;Initial Catalog=INEFABLE;User ID=sa;Password=DB@gr0V3rs@"))
                 {
                     CONECT.Open();
-                    using (SqlCommand COMANDO = new SqlCommand(@"SELECT  T1.DocNum, T1.CardCode, T1.CardName,CONVERT(varchar,T1.DocDate,23) as date , T1.DocTotal,T2.U_NAME, T3.ZONA, T3.COD_ZONA, T3.Division_Comercial FROM  [VM-SQL\SAP].AGROVERSA_PRODUCTIVA.dbo.OINV AS T1 INNER JOIN [VM-SQL\SAP].AGROVERSA_PRODUCTIVA.dbo.OUSR AS T2 ON T1.UserSign = T2.USERID INNER JOIN vwCAT_AGENTES AS T3 ON T3.COD_ZONA = T1.SlpCode WHERE YEAR(T1.DocDate)>=2019 and T1.CANCELED = 'N'", CONECT))
+                    using (SqlCommand COMANDO = new SqlCommand(@"SELECT  T1.DocNum, T1.CardCode, T1.CardName,CONVERT(varchar,T1.DocDate,23) as date,(select STUFF(STUFF(STUFF(T0.CreateTS, 1, 0, REPLICATE('0', 6 - LEN(T0.createTS))),3,0,':'),6,0,':')from [VM-SQL\SAP].AGROVERSA_PRODUCTIVA.dbo.OINV T0 WHERE T1.DocNum = T0.DocNum) as hora , T1.DocTotal,T2.U_NAME, T3.ZONA, T3.COD_ZONA, T3.Division_Comercial FROM  [VM-SQL\SAP].AGROVERSA_PRODUCTIVA.dbo.OINV AS T1 INNER JOIN [VM-SQL\SAP].AGROVERSA_PRODUCTIVA.dbo.OUSR AS T2 ON T1.UserSign = T2.USERID INNER JOIN vwCAT_AGENTES AS T3 ON T3.COD_ZONA = T1.SlpCode WHERE YEAR(T1.DocDate)>=2019 and T1.CANCELED = 'N'", CONECT))
 
 
                     {
