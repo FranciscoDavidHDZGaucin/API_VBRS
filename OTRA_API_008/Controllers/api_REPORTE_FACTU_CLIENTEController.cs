1b4e22e707a543d0f4f4eb1eb1404452c9c4734e
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2021-04-30 15:44:21 -0500

    API FACTURAS CLIENTE DAFH

---
+++ OTRA_API_008/Controllers/api_REPORTE_FACTU_CLIENTEController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class api_REPORTE_FACTU_CLIENTEController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            api_REPORTE_FACTU_CLIENTE REPORTE = new api_REPORTE_FACTU_CLIENTE();
+            JArray result = new JArray();
+            result = REPORTE.REPORTE();
+            return result;
+        }
+    }
+
+}
