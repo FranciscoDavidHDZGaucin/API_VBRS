d9c9ab6c5c66d63126dc780c479f43b724069cd5
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2021-07-01 13:24:57 -0500

    API sondeo y participacion DAFH

---
+++ OTRA_API_008/Controllers/api_PARTICIPACION_CULTIVOSController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class api_PARTICIPACION_CULTIVOSController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            api_PARTICIPACION_CULTIVOS TB = new api_PARTICIPACION_CULTIVOS();
+            JArray result = new JArray();
+            result = TB.REPORTE();
+            return result;
+        }
+    }
+}
