3465f1368681401328f8570c43b2bf4357036499
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2021-07-28 11:05:25 -0500

    api reporte autorizaciones ordventa DAFH

---
+++ OTRA_API_008/Controllers/api_REPORTE_AUTO_ORDVENTAController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class api_REPORTE_AUTO_ORDVENTAController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            api_REPORTE_AUTO_ORDVENTA REPORTE = new api_REPORTE_AUTO_ORDVENTA();
+            JArray result = new JArray();
+            result = REPORTE.REPORTE();
+            return result;
+        }
+    }
+}
