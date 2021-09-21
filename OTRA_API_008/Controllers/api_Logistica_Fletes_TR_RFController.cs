b6f30dda9b445176f37f12eee2be567e730112b2
David Alberto Figueroa Herrera <dfigueroa@agroversa.mx>
2021-09-21 17:17:16 -0500

    API LOGISTICA RF Y TR DAFH

---
+++ OTRA_API_008/Controllers/api_Logistica_Fletes_TR_RFController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OTRA_API_008.Models;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OTRA_API_008.Controllers
+{
+    public class api_Logistica_Fletes_TR_RFController : ApiController
+    {
+
+        public Newtonsoft.Json.Linq.JArray Get()
+        {
+            api_Logistica_Fletes_TR_RF FLETES = new api_Logistica_Fletes_TR_RF();
+            JArray result = new JArray();
+            result = FLETES.Fletes();
+            return result;
+        }
+    }
+}
--- OTRA_API_008/Models/api_Logistica_Fletes_TR_RF.cs
+++ OTRA_API_008/Models/api_Logistica_Fletes_TR_RF.cs
@@ -7,10 +7,9 @@ using Newtonsoft.Json.Linq;
 using System.Data;
 using System.Data.SqlClient;
 
-
 namespace OTRA_API_008.Models
 {
-    public class api_Logistica_Fletes_Folios
+    public class api_Logistica_Fletes_TR_RF
     {
         private SqlDataReader drd;
 
@@ -25,7 +24,7 @@ namespace OTRA_API_008.Models
                 using (SqlConnection CONECT = new SqlConnection(@"Data Source=192.168.101.154;Initial Catalog=JUPITER;User ID=sa;Password=DB@gr0V3rs@"))
                 {
                     CONECT.Open();
-                    using (SqlCommand COMANDO = new SqlCommand("select * from  OPENQUERY([MYSQL_SRV105],'select t1.n_remision,t2.folio, t1.entrega, t4.empresa, t3.comentarios, t3.km_prom_real, t3.flete_prov, t3.maniobras, t3.tot_peso, t3.tot_facturas, (t3.flete_prov*100)/t3.tot_facturas as gasto_flete, t3.flete_prov/t3.km_prom as costo_km, t3.envio_autoriza, t3.fecha, t2.fecha_entrega, t2.fecha_promesa, t2.fecha_salida, t1.fecha_alta, t3.km_prom from pedidos.detalle_pedido t1 join pedidos.envios_det_productos_rf t2 on t1.id_detalle = t2.id_detalle join pedidos.envios_encabeza t3 on t2.folio = t3.folio join pedidos.transportes t4 on t3.n_proveedor = t4.id where t1.fecha_alta >=2021-01-01; ' )", CONECT))
+                    using (SqlCommand COMANDO = new SqlCommand("select t1.folio, t4.empresa, t1.comentarios, t1.km_prom, t1.km_prom_real, t1.flete_prov, t1.maniobras, t1.tot_peso, t1.tot_cajas, t1.tot_facturas, (t1.flete_prov*100)/t1.tot_facturas as gasto_flete, t1.flete_prov/t1.km_prom as costo_km, t1.envio_autoriza, t1.fecha, t2.fecha_entrega, t2.fecha_promesa, t2.fecha_salida, t2.fecha_promesa2, t2.tipo_doc from pedidos.envios_encabeza t1 join pedidos.envios_det_productos_rf t2 on t2.folio = t1.folio join pedidos.transferencias t3 on t3.movimiento = t2.factura join pedidos.transportes t4 on t4.id = t1.n_proveedor where t1.fecha>= 2021-09-01; ' )", CONECT))
 
 
                     {
